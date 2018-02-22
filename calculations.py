655557d35576865d58f1da2fdd4a73b4206c521c
Bladesmc <Bladesmc@gmail.com>
2018-02-22 14:02:31 -0800

    Lots of neat changes

---
+++ calculations.py
@@ -0,0 +1,78 @@
+# Copyright 2018 Brennan McMicking
+# This file does all the fun math and returns it back to the command line or GUI.
+import factorial as f
+
+def calculate(inp):
+    inp = inp.replace(" ", "")
+    o = ""
+    if inp[-1] == "!":
+        o = get_factorial(inp[:-1])
+    elif inp.find("^") is not -1:
+        o = get_exponent(inp)
+    elif inp.find("%") is not -1:
+        o = get_modulo(inp)
+    else:
+        o = "[ERROR] Could not determine mathematical function."
+    return str(o)
+
+
+def get_factorial(n):
+    error = False
+    o = ""
+    ni = int()
+    try:
+        ni = int(n)
+    except ValueError:
+        error = True
+        o = "[ERROR] Invalid number. {fact}"
+
+    if not error:
+        try:
+            o = n + "! = " + str(f.factorial(ni))
+        except RuntimeError:
+            o = "[ERROR] Number exceeded maximum size. {fact}"
+
+    return o
+
+
+def get_exponent(s):
+    error = False
+    pwr = s.find("^")
+    base, exp = int(),int()
+    o = ""
+    try:
+        base = int(s[:pwr])
+    except ValueError:
+        o = "[ERROR] Invalid base. {exp}"
+        error = True
+    try:
+        exp = int(s[pwr + 1:])
+    except ValueError:
+        o = "[ERROR] Invalid exponent. {exp}"
+        error = True
+    if not error:
+        o = str(base ** exp)
+
+    return o
+
+
+def get_modulo(s):
+    error = False
+    mod = s.find("%")
+    dividend, divisor = int(),int()
+    o = ""
+    try:
+        dividend = int(s[:mod])
+    except ValueError:
+        o = "[ERROR] Invalid dividend. {mod}"
+        error = True
+    try:
+        divisor = int(s[mod + 1:])
+    except ValueError:
+        o = "[ERROR] Invalid divisor. {mod}"
+        error = True
+
+    if not error:
+        o = str(dividend % divisor)
+
+    return o
--- gui.py
+++ gui.py
@@ -1,55 +1,16 @@
 # Copyright 2018 Brennan McMicking
-# TODO: support multiple operations at once
+# TODO: support multiple operations at once WHICH IS NOT EASY SO IT MIGHT NOT GET DONE
 
-import factorial as f
+import calculations as calc
 from Tkinter import *
 
 
 class Application(Frame):
-    def get_factorial(self, n):
-        error = False
-        try:
-            ni = int(n)
-        except ValueError:
-            error = True
-            o = "[ERROR] Invalid number. {fact}"
-
-        if not error:
-            try:
-                o = n + "! = " + str(f.factorial(ni))
-            except RuntimeError:
-                o = "[ERROR] Number exceeded maximum size. {fact}"
-
-        self.v.set(o)
-
-    def get_exponent(self, s):
-        error = False
-        pwr = s.find("^")
-        try:
-            base = int(s[:pwr])
-        except ValueError:
-            o = "[ERROR] Invalid base. {exp}"
-            error = True
-        try:
-            exp = int(s[pwr + 1:])
-        except ValueError:
-            o = "[ERROR] Invalid exponent. {exp}"
-            error = True
-        if not error:
-            o = str(base ** exp)
-
-        self.v.set(o)
-
     def calculate(self):
         inp = self.INPUT.get()
-        if inp[-1] == "!":
-            self.get_factorial(inp[:-1])
-        elif inp.find("^") is not -1:
-            self.get_exponent(inp)
-        else:
-            self.v.set("[ERROR] Could not determine mathematical function.")
+        self.v.set(calc.calculate(inp))
 
-    def createWidgets(self):
+    def createwidgets(self):
         self.GETFACT = Button(self, text="Calculate", fg="green", command=self.calculate)
         self.GETFACT.pack({"side": "left"})
 
@@ -64,11 +25,12 @@ class Application(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        self.createWidgets()
+        self.createwidgets()
 
 
 root = Tk(screenName="Calculator", baseName="Calculator", className="Calculator")
 # root.geometry("320x180")
 root.minsize(320, 40)
+root.maxsize(1920, 1080)
 app = Application(root)
 app.mainloop()
